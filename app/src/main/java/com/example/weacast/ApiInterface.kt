8fba9b124cbcf59612f11100c2e48baaac1c41b2
Tejas Nagpure <tejasnagpure4404@gmail.com>
2025-05-19 09:02:50 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/weacast/ApiInterface.kt
@@ -0,0 +1,15 @@
+package com.example.weacast
+
+import androidx.core.text.util.LocalePreferences.TemperatureUnit.TemperatureUnits
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("weather")
+    fun getWeatherData(
+        @Query("q") city:String,
+        @Query("appid") appid:String,
+        @Query("units") units:String
+    ) : Call<WeatherApp>
+}
---
+++ app/src/main/java/com/example/weacast/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.weacast
+
+data class Clouds(
+    val all: Int
+)
